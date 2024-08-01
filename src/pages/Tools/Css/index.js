d2cca74a226a72aa8b2e74676381508b39159a99
Oslonline <Oslo418@yandex.ru>
2024-08-01 16:02:43 +0200

    fix

--- src/pages/Tools/Css/index.js
+++ src/pages/Tools/Css/index.js
@@ -1,2 +1,2 @@
-export { default as Gradient } from "./Gradient.jsx";
-export { default as BoxShadow } from "./BoxShadow.jsx";
+export { default as Gradient } from "./Gradient";
+export { default as BoxShadow } from "./Boxshadow";
