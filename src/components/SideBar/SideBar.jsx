af49d89b360467e18fbe25636be24692fd9d7af8
Soudeh <babaeizadeh.s@gmail.com>
2024-08-16 20:30:06 +0330

    improve sidebar responsive on mobile

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -9,7 +9,7 @@ function SideBar() {
     <div
       className={`sticky left-0 h-full border-r-2 border-gray-200 bg-gray-50 transition-transform duration-300 ${isOpen ? "w-56" : "w-16"} md:w-56`}
     >
-      <div className="flex  items-center justify-between border-b-2 border-gray-200 md:p-6 sm:p-3">
+      <div className="flex  items-center justify-center border-b-2 border-gray-200 md:p-6 sm:p-3">
         <Link to={"/"} className="h-full p-2 sm:p-1 md:p-4">
           <img
             className="w-8 sm:w-10 md:w-20 lg:w-32"
@@ -20,11 +20,11 @@ function SideBar() {
       </div>
       {/* Show sidebar nav only if isOpen is true */}
 
-      <nav className="flex h-5/6 flex-col">
+      <nav className="flex h-5/6 flex-col overflow-hidden">
         <SidebarNav isOpen={isOpen} />
       </nav>
       {isOpen && (
-        <nav className="flex h-5/6 flex-col">
+        <nav className="flex h-5/6 flex-col overflow-hidden">
           <SidebarNav isOpen={isOpen} />
         </nav>
       )}
