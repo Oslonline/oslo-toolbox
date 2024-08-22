94c93496735da36377b3343ec0ba20fc5b780e56
Soudeh <babaeizadeh.s@gmail.com>
2024-08-22 22:33:22 +0330

    fix mobile sidebar dropdown

--- src/components/SideBar/SidebarNav.jsx
+++ src/components/SideBar/SidebarNav.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import SidebarLinks from "./SidebarLinks";
 import SidebarDropdown from "./SidebarDropdown";
 
@@ -14,6 +15,7 @@ import { LuImage } from "react-icons/lu";
 import { IoSpeedometer, IoTimerSharp } from "react-icons/io5";
 
 function SidebarNav({ isOpen }) {
+  const navigate = useNavigate();
   const [mainDropdowns, setMainDropdowns] = useState({
     generators: false,
     file_convert: false,
@@ -53,24 +55,29 @@ function SidebarNav({ isOpen }) {
     });
   };
 
+  const handleRedirect = (path) => {
+    navigate(path); // Redirect to the specified path
+    // closeAllDropdowns(); // Close all dropdowns after redirect
+  };
+
   return (
     <div className="h-full w-full p-3 pr-0">
       <div className="flex h-fit w-full flex-col gap-2 overflow-y-auto pr-3">
-        <SidebarLinks link={"/"} icon={<FaHome />} linkname={"Home"} onClick={closeAllDropdowns} isOpen={isOpen} />
+        <SidebarLinks link={"/"} icon={<FaHome />} linkname={"Home"} onClick={() => handleRedirect("/")} isOpen={isOpen} />
         <SidebarLinks link={"/About"} icon={<GrCircleInformation />} linkname={"About"} onClick={closeAllDropdowns} isOpen={isOpen} />
         <hr />
-        <SidebarDropdown label="Generators" icon={<FaGear />} isOpen={mainDropdowns.generators} onClick={() => toggleMainDropdown("generators")}>
+        <SidebarDropdown label="Generators" icon={<FaGear />} isOpen={mainDropdowns.generators} onClick={() => handleRedirect("generators")}>
           <SidebarLinks link={"/Generators/Password"} icon={<FaUnlockAlt />} linkname={"Password"} />
           <SidebarLinks link={"/Generators/Lorem"} icon={<TbCircleLetterL />} linkname={"Lorem Ipsum"} />
           <SidebarLinks link={"/Generators/QrCode"} icon={<FaQrcode />} linkname={"QR Code"} />
           <SidebarLinks link={"/Generators/Readme"} icon={<CgReadme />} linkname={"Readme"} />
         </SidebarDropdown>
         <hr />
-        <SidebarDropdown label="Files Converter" icon={<SiConvertio />} isOpen={mainDropdowns.file_convert} onClick={() => toggleMainDropdown("file_convert")}>
+        <SidebarDropdown label="Files Converter" icon={<SiConvertio />} isOpen={mainDropdowns.file_convert} onClick={() => handleRedirect("FilesConvert")}>
           <SidebarLinks link={"/FilesConvert/Images"} icon={<LuImage />} linkname={"Images"} />
         </SidebarDropdown>
         <hr />
-        <SidebarDropdown label="Units Converter" icon={<FaBalanceScale />} isOpen={mainDropdowns.convert} onClick={() => toggleMainDropdown("convert")}>
+        <SidebarDropdown label="Units Converter" icon={<FaBalanceScale />} isOpen={mainDropdowns.convert} onClick={() => handleRedirect("UnitsConvert")}>
           <SidebarLinks link={"/UnitsConvert/Mass"} icon={<FaWeightHanging />} linkname={"Mass"} />
           <SidebarLinks link={"/UnitsConvert/Length"} icon={<FaRuler />} linkname={"Length"} />
           <SidebarLinks link={"/UnitsConvert/Area"} icon={<FaRulerCombined />} linkname={"Area"} />
@@ -80,12 +87,12 @@ function SidebarNav({ isOpen }) {
           <SidebarLinks link={"/UnitsConvert/Time"} icon={<IoTimerSharp />} linkname={"Time"} />
         </SidebarDropdown>
         <hr />
-        <SidebarDropdown label="Security" icon={<MdSecurity />} isOpen={mainDropdowns.security} onClick={() => toggleMainDropdown("security")}>
+        <SidebarDropdown label="Security" icon={<MdSecurity />} isOpen={mainDropdowns.security} onClick={() => handleRedirect("security")}>
           <SidebarLinks link={"/Security/IpLookup"} icon={<TbMapPinQuestion />} linkname={"IP Lookup"} />
           <SidebarLinks link={"/Security/Hash"} icon={<FaHashtag />} linkname={"Hash Generator"} />
         </SidebarDropdown>
         <hr />
-        <SidebarDropdown label="Css" icon={<FaCss3Alt />} isOpen={mainDropdowns.css} onClick={() => toggleMainDropdown("css")}>
+        <SidebarDropdown label="Css" icon={<FaCss3Alt />} isOpen={mainDropdowns.css} onClick={() => handleRedirect("css")}>
           <SidebarLinks link={"/Css/Gradient"} icon={<MdGradient />} linkname={"Gradient gen"} />
           <SidebarLinks link={"/Css/Box-shadow"} icon={<BsShadows />} linkname={"Box-shadow gen"} />
         </SidebarDropdown>
