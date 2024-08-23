2ea38d868be43229b4fa5c50336b6e45fc3d9f6a
Soudeh <babaeizadeh.s@gmail.com>
2024-08-23 20:59:18 +0330

    fix sidebar problem for mobile and desktop

--- src/components/SideBar/SidebarNav.jsx
+++ src/components/SideBar/SidebarNav.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import SidebarLinks from "./SidebarLinks";
 import SidebarDropdown from "./SidebarDropdown";
 
-import { FaCss3Alt, FaHome, FaWeightHanging, FaRuler, FaThermometerHalf, FaRulerCombined, FaCube, FaUnlockAlt, FaQrcode, FaHashtag, FaBalanceScale, FaRegFileAlt } from "react-icons/fa";
+import { FaCss3Alt, FaHome, FaWeightHanging, FaRuler, FaThermometerHalf, FaRulerCombined, FaCube, FaUnlockAlt, FaQrcode, FaHashtag, FaBalanceScale } from "react-icons/fa";
 import { GrCircleInformation } from "react-icons/gr";
 import { TbCircleLetterL, TbMapPinQuestion } from "react-icons/tb";
 import { MdGradient, MdSecurity } from "react-icons/md";
@@ -24,26 +24,22 @@ function SidebarNav({ isOpen }) {
     css: false,
   });
 
-  // const [subDropdowns, setSubDropdowns] = useState({
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
-  // });
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const toggleMainDropdown = (dropdown) => {
     setMainDropdowns((prevState) => ({
       ...Object.fromEntries(Object.keys(prevState).map((key) => [key, key === dropdown ? !prevState[key] : false])),
     }));
-
-    // setSubDropdowns({
-
-    // });
   };
-
-  // const toggleSubDropdown = (dropdown) => {
-  //     setSubDropdowns((prevState) => ({
-  //         ...prevState,
-  //         [dropdown]: !prevState[dropdown],
-  //     }));
-  // };
 
   const closeAllDropdowns = () => {
     setMainDropdowns({
@@ -56,8 +52,7 @@ function SidebarNav({ isOpen }) {
   };
 
   const handleRedirect = (path) => {
-    navigate(path); // Redirect to the specified path
-    // closeAllDropdowns(); // Close all dropdowns after redirect
+    navigate(path);
   };
 
   return (
@@ -66,38 +61,87 @@ function SidebarNav({ isOpen }) {
         <SidebarLinks link={"/"} icon={<FaHome />} linkname={"Home"} onClick={() => handleRedirect("/")} isOpen={isOpen} />
         <SidebarLinks link={"/About"} icon={<GrCircleInformation />} linkname={"About"} onClick={closeAllDropdowns} isOpen={isOpen} />
         <hr />
-        <SidebarDropdown label="Generators" icon={<FaGear />} isOpen={mainDropdowns.generators} onClick={() => handleRedirect("generators")}>
-          <SidebarLinks link={"/Generators/Password"} icon={<FaUnlockAlt />} linkname={"Password"} />
-          <SidebarLinks link={"/Generators/Lorem"} icon={<TbCircleLetterL />} linkname={"Lorem Ipsum"} />
-          <SidebarLinks link={"/Generators/QrCode"} icon={<FaQrcode />} linkname={"QR Code"} />
-          <SidebarLinks link={"/Generators/Readme"} icon={<CgReadme />} linkname={"Readme"} />
-        </SidebarDropdown>
+
+        {/* Generators Dropdown */}
+        {isMobile ? (
+          <div onClick={() => handleRedirect("/Generators")} className="flex h-12 cursor-pointer items-center justify-center">
+            <FaGear />
+          </div>
+        ) : (
+          <SidebarDropdown label="Generators" icon={<FaGear />} isOpen={mainDropdowns.generators} onClick={() => toggleMainDropdown("generators")}>
+            <>
+              <SidebarLinks link={"/Generators/Password"} icon={<FaUnlockAlt />} linkname={"Password"} />
+              <SidebarLinks link={"/Generators/Lorem"} icon={<TbCircleLetterL />} linkname={"Lorem Ipsum"} />
+              <SidebarLinks link={"/Generators/QrCode"} icon={<FaQrcode />} linkname={"QR Code"} />
+              <SidebarLinks link={"/Generators/Readme"} icon={<CgReadme />} linkname={"Readme"} />
+            </>
+          </SidebarDropdown>
+        )}
         <hr />
-        <SidebarDropdown label="Files Converter" icon={<SiConvertio />} isOpen={mainDropdowns.file_convert} onClick={() => handleRedirect("FilesConvert")}>
-          <SidebarLinks link={"/FilesConvert/Images"} icon={<LuImage />} linkname={"Images"} />
-        </SidebarDropdown>
+
+        {/* Files Converter Dropdown */}
+        {isMobile ? (
+          <div onClick={() => handleRedirect("/FilesConvert")} className="flex h-12 cursor-pointer items-center justify-center">
+            <SiConvertio />
+          </div>
+        ) : (
+          <SidebarDropdown label="Files Converter" icon={<SiConvertio />} isOpen={mainDropdowns.file_convert} onClick={() => toggleMainDropdown("file_convert")}>
+            <SidebarLinks link={"/FilesConvert/Images"} icon={<LuImage />} linkname={"Images"} />
+          </SidebarDropdown>
+        )}
         <hr />
-        <SidebarDropdown label="Units Converter" icon={<FaBalanceScale />} isOpen={mainDropdowns.convert} onClick={() => handleRedirect("UnitsConvert")}>
-          <SidebarLinks link={"/UnitsConvert/Mass"} icon={<FaWeightHanging />} linkname={"Mass"} />
-          <SidebarLinks link={"/UnitsConvert/Length"} icon={<FaRuler />} linkname={"Length"} />
-          <SidebarLinks link={"/UnitsConvert/Area"} icon={<FaRulerCombined />} linkname={"Area"} />
-          <SidebarLinks link={"/UnitsConvert/Volume"} icon={<FaCube />} linkname={"Volume"} />
-          <SidebarLinks link={"/UnitsConvert/Temperature"} icon={<FaThermometerHalf />} linkname={"Temperature"} />
-          <SidebarLinks link={"/UnitsConvert/Speed"} icon={<IoSpeedometer />} linkname={"Speed"} />
-          <SidebarLinks link={"/UnitsConvert/Time"} icon={<IoTimerSharp />} linkname={"Time"} />
-        </SidebarDropdown>
+
+        {/* Units Converter Dropdown */}
+        {isMobile ? (
+          <div onClick={() => handleRedirect("/UnitsConvert")} className="flex h-12 cursor-pointer items-center justify-center">
+            <FaBalanceScale />
+          </div>
+        ) : (
+          <SidebarDropdown label="Units Converter" icon={<FaBalanceScale />} isOpen={mainDropdowns.convert} onClick={() => toggleMainDropdown("convert")}>
+            <>
+              <SidebarLinks link={"/UnitsConvert/Mass"} icon={<FaWeightHanging />} linkname={"Mass"} />
+              <SidebarLinks link={"/UnitsConvert/Length"} icon={<FaRuler />} linkname={"Length"} />
+              <SidebarLinks link={"/UnitsConvert/Area"} icon={<FaRulerCombined />} linkname={"Area"} />
+              <SidebarLinks link={"/UnitsConvert/Volume"} icon={<FaCube />} linkname={"Volume"} />
+              <SidebarLinks link={"/UnitsConvert/Temperature"} icon={<FaThermometerHalf />} linkname={"Temperature"} />
+              <SidebarLinks link={"/UnitsConvert/Speed"} icon={<IoSpeedometer />} linkname={"Speed"} />
+              <SidebarLinks link={"/UnitsConvert/Time"} icon={<IoTimerSharp />} linkname={"Time"} />
+            </>
+          </SidebarDropdown>
+        )}
         <hr />
-        <SidebarDropdown label="Security" icon={<MdSecurity />} isOpen={mainDropdowns.security} onClick={() => handleRedirect("security")}>
-          <SidebarLinks link={"/Security/IpLookup"} icon={<TbMapPinQuestion />} linkname={"IP Lookup"} />
-          <SidebarLinks link={"/Security/Hash"} icon={<FaHashtag />} linkname={"Hash Generator"} />
-        </SidebarDropdown>
+
+        {/* Security Dropdown */}
+        {isMobile ? (
+          <div onClick={() => handleRedirect("/Security")} className="flex h-12 cursor-pointer items-center justify-center">
+            <MdSecurity />
+          </div>
+        ) : (
+          <SidebarDropdown label="Security" icon={<MdSecurity />} isOpen={mainDropdowns.security} onClick={() => toggleMainDropdown("security")}>
+            <>
+              <SidebarLinks link={"/Security/IpLookup"} icon={<TbMapPinQuestion />} linkname={"IP Lookup"} />
+              <SidebarLinks link={"/Security/Hash"} icon={<FaHashtag />} linkname={"Hash Generator"} />
+            </>
+          </SidebarDropdown>
+        )}
         <hr />
-        <SidebarDropdown label="Css" icon={<FaCss3Alt />} isOpen={mainDropdowns.css} onClick={() => handleRedirect("css")}>
-          <SidebarLinks link={"/Css/Gradient"} icon={<MdGradient />} linkname={"Gradient gen"} />
-          <SidebarLinks link={"/Css/Box-shadow"} icon={<BsShadows />} linkname={"Box-shadow gen"} />
-        </SidebarDropdown>
+
+        {/* CSS Dropdown */}
+        {isMobile ? (
+          <div onClick={() => handleRedirect("/Css")} className="flex h-12 cursor-pointer items-center justify-center">
+            <FaCss3Alt />
+          </div>
+        ) : (
+          <SidebarDropdown label="Css" icon={<FaCss3Alt />} isOpen={mainDropdowns.css} onClick={() => toggleMainDropdown("css")}>
+            <>
+              <SidebarLinks link={"/Css/Gradient"} icon={<MdGradient />} linkname={"Gradient gen"} />
+              <SidebarLinks link={"/Css/Box-shadow"} icon={<BsShadows />} linkname={"Box-shadow gen"} />
+            </>
+          </SidebarDropdown>
+        )}
       </div>
     </div>
   );
 }
+
 export default SidebarNav;
