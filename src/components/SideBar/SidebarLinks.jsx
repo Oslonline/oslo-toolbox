8ee6a9c55f93e0582ee8acdc5ee1975a6bb3ad7e
Oslonline <Oslo418@yandex.ru>
2024-09-24 12:20:27 +0200

    fix: minor styling changes

--- src/components/SideBar/SidebarLinks.jsx
+++ src/components/SideBar/SidebarLinks.jsx
@@ -5,7 +5,7 @@ function SidebarLinks({ link, icon, linkname, onClick }) {
   const isActive = location.pathname === link;
 
   return (
-    <NavLink to={link} onClick={onClick} className={`flex items-center gap-2 rounded-md px-3 py-2 text-gray-950 hover:bg-gray-200 dark:text-gray-100 dark:hover:bg-gray-800 ${isActive ? "bg-gray-800 text-white dark:bg-gray-200 dark:text-gray-950 hover:dark:bg-gray-200" : ""}`}>
+    <NavLink to={link} onClick={onClick} className={`flex items-center gap-2 rounded-md px-3 py-2 text-gray-950 hover:bg-gray-200 dark:text-gray-100 dark:hover:bg-gray-800 ${isActive ? "bg-gray-800 text-white dark:bg-gray-200 dark:text-gray-950 hover:bg-gray-800 hover:dark:bg-gray-200" : ""}`}>
       <div>{icon}</div>
       <div className={`font-medium ${isActive ? "underline" : ""} hidden md:block`}>{linkname}</div>
     </NavLink>
