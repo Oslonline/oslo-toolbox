d2d1a409ae2df7ec5c87a68b697797b4e3e4787a
Oslonline <Oslo418@yandex.ru>
2024-11-28 22:29:13 +0100

    fix: remove useless react import

--- src/pages/Tools/FilesConvert/Images.jsx
+++ src/pages/Tools/FilesConvert/Images.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 import imageCompression from "browser-image-compression";
 import { saveAs } from "file-saver";
