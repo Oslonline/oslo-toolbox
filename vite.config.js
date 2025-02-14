8a1b2c6fc33fb86eb798a1492a9ecd83f3afa590
Oslonline <Oslo418@yandex.ru>
2025-02-14 18:27:20 +0100

    fix: add manualChunks option because of Javascript chunks size on build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['react-router-dom', 'react-icons', 'browser-image-compression', 'file-saver'],
+        },
+      },
+    },
+  },
 });
