3c14e721905bae56563b1742ddfeb484e06c885b
Oslonline <Oslo418@yandex.ru>
2025-02-14 18:36:44 +0100

    fix: handle drag and drop file upload

--- src/components/commons/ImageUploader.jsx
+++ src/components/commons/ImageUploader.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { IoCloudUploadOutline } from "react-icons/io5";
 
 const ImageUploader = ({ onFileSelect }) => {
-  const [selectedFile, setSelectedFile] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
+  const [isDragging, setIsDragging] = useState(false);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -12,33 +12,43 @@ const ImageUploader = ({ onFileSelect }) => {
 
   const handleDrop = (event) => {
     event.preventDefault();
+    setIsDragging(false);
     const file = event.dataTransfer.files[0];
     validateFile(file);
   };
 
   const validateFile = (file) => {
     if (file && file.type.startsWith("image/")) {
-      setSelectedFile(file);
-      setErrorMessage("");
       onFileSelect(file);
     } else {
       setErrorMessage("Please select a valid image file.");
-      setSelectedFile(null);
     }
   };
 
   const handleDragOver = (event) => {
     event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
   };
 
   return (
     <div className="w-full">
-      <div onClick={() => document.getElementById("file-input").click()} className="flex w-full cursor-pointer flex-col items-center justify-center rounded-md bg-orange-400 p-4 text-center text-white duration-200 hover:bg-orange-600 dark:bg-orange-600 dark:hover:bg-orange-400">
+      <div
+        onClick={() => document.getElementById("file-input").click()}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        className={`flex w-full cursor-pointer flex-col items-center justify-center rounded-md p-4 text-center text-white duration-200 ${isDragging ? "bg-orange-600 dark:bg-orange-400" : "bg-orange-400 dark:bg-orange-600"}`}
+      >
         <IoCloudUploadOutline className="text-9xl" />
         <p className="text-lg">Drag here or click to import your image</p>
         <p className="mt-2 text-center text-xs">SVG, PNG, JPG, WEBP, or BMP (MAX. 20MB).</p>
       </div>
       <input id="file-input" type="file" accept="image/*" onChange={handleFileChange} className="hidden" />
+      {errorMessage && <p className="mt-2 text-red-500">{errorMessage}</p>}
     </div>
   );
 };
