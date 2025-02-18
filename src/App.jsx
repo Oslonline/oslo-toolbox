3b112fabc47511a0ea3a631f48128eff0117aa51
Oslonline <Oslo418@yandex.ru>
2025-02-18 20:13:15 +0100

    fix: import case error on production build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
 import SearchModal from "./components/header/SearchModal";
 import { Outlet } from "react-router-dom";
