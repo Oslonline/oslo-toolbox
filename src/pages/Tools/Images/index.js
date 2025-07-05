1ca1c1e65ced51aaea9401b4d585cdf637c24014
Oslonline <Oslo418@yandex.ru>
2025-07-05 20:27:06 +0200

    feat: Add Blur component export to Images tools

--- src/pages/Tools/Images/index.js
+++ src/pages/Tools/Images/index.js
@@ -1,3 +1,4 @@
 export { default as ImagesConverter } from "./ImagesConverter";
 export { default as Dither } from "./Dither";
 export { default as Noise } from "./Noise";
+export { default as Blur } from "./Blur";
